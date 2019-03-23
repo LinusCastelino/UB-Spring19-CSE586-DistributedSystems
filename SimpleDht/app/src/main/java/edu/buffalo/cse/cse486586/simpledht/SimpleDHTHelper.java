fbce9beb597145bf77b6bd9ef52d2ef7c49cc83e
Linus Castelino <castelinolinus@gmail.com>
2019-03-23 02:12:29 -0400

    PA3 base created

---
+++ SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDHTHelper.java
@@ -0,0 +1,60 @@
+package edu.buffalo.cse.cse486586.simpledht;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SimpleDHTHelper extends SQLiteOpenHelper {
+
+    private static final Integer DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "SimpleDHT.db";
+
+    private static final String TABLE_NAME = "DHT_TABLE";
+    private static final String KEY_COLUMN_NAME = "key";
+    private static final String VALUE_COLUMN_NAME = "value";
+
+    private static final String CREATE_TABLE_QUERY = "CREATE TABLE " + TABLE_NAME +
+                                                     " (" + KEY_COLUMN_NAME + " text PRIMARY KEY, " +
+                                                     VALUE_COLUMN_NAME + " text)";
+    private static final String DROP_TABLE_QUERY = "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    private static final String LOCAL_PAIRS_QUERY = "@";
+
+    public SimpleDHTHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_TABLE_QUERY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(DROP_TABLE_QUERY);
+        onCreate(sqLiteDatabase);
+    }
+
+    public void insert(ContentValues contentValues){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.insertWithOnConflict(TABLE_NAME, null, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
+    }    //insert()
+
+    public Cursor query(String queryKey){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        if(queryKey.equals(LOCAL_PAIRS_QUERY)){
+            return null;
+        }
+        else{
+            String selection = KEY_COLUMN_NAME + " = ?";
+
+            return db.query(TABLE_NAME, new String[]{KEY_COLUMN_NAME, VALUE_COLUMN_NAME}, selection,
+                    new String[]{queryKey}, null, null, null);
+        }
+
+    }    //query()
+
+}    //SimpleDHTHelper
--- SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
+++ SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
@@ -1,15 +1,28 @@
 package edu.buffalo.cse.cse486586.simpledht;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Formatter;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 public class SimpleDhtProvider extends ContentProvider {
+    private static final String TAG = SimpleDhtProvider.class.getSimpleName();
+    private static final Integer SERVER_PORT = 10000;
+
+    private static final String LOCAL_PAIRS_QUERY = "@";
+    private static final String ALL_PAIRS_QUERY = "*";
+
+    private String selfPort;
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
@@ -26,12 +39,30 @@ public class SimpleDhtProvider extends ContentProvider {
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         // TODO Auto-generated method stub
-        return null;
+        Log.v(TAG, values.toString());
+
+        SimpleDHTHelper dhtHelper = new SimpleDHTHelper(this.getContext());
+        dhtHelper.insert(values);
+
+        return uri;
     }
 
     @Override
     public boolean onCreate() {
         // TODO Auto-generated method stub
+
+        TelephonyManager tel = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
+        selfPort = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+
+        try{
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
+        }
+        catch (IOException ioe){
+            Log.e(TAG, "Error encountered while creating server socket");
+            Log.e(TAG, ioe.getMessage());
+            return false;
+        }
         return false;
     }
 
@@ -39,7 +70,10 @@ public class SimpleDhtProvider extends ContentProvider {
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
             String sortOrder) {
         // TODO Auto-generated method stub
-        return null;
+        Log.v(TAG, selection);
+
+        SimpleDHTHelper dhtHelper = new SimpleDHTHelper(this.getContext());
+        return dhtHelper.query(selection);
     }
 
     @Override
@@ -56,5 +90,22 @@ public class SimpleDhtProvider extends ContentProvider {
             formatter.format("%02x", b);
         }
         return formatter.toString();
+    }
+
+
+    private class ServerTask extends AsyncTask<ServerSocket, String, Void>{
+
+        @Override
+        protected Void doInBackground(ServerSocket... sockets) {
+            return null;
+        }
+    }    //ServerTask
+
+    private class ClientTask extends AsyncTask<String, Void, Void>{
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            return null;
+        }
     }
 }
