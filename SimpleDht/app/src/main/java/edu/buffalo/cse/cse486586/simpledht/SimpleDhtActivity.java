063eec93b5b849f5026e9d9de11a2b666e31816d
Linus Castelino <castelinolinus@gmail.com>
2019-03-31 19:26:56 -0400

    Implemented test buttons

--- SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtActivity.java
+++ SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtActivity.java
@@ -1,9 +1,13 @@
 package edu.buffalo.cse.cse486586.simpledht;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SimpleDhtActivity extends Activity {
@@ -13,10 +17,39 @@ public class SimpleDhtActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_dht_main);
         
-        TextView tv = (TextView) findViewById(R.id.textView1);
+        final TextView tv = (TextView) findViewById(R.id.textView1);
         tv.setMovementMethod(new ScrollingMovementMethod());
         findViewById(R.id.button3).setOnClickListener(
                 new OnTestClickListener(tv, getContentResolver()));
+
+        final Uri CONTENT_PROVIDER_URI = Uri.parse("content://edu.buffalo.cse.cse486586.simpledht.provider");
+        final String LOCAL_PAIRS_QUERY = "@";
+        final String ALL_PAIRS_QUERY = "*";
+
+        Button lDump = (Button)findViewById(R.id.button1);
+        Button gDump = (Button)findViewById(R.id.button2);
+
+        lDump.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Cursor cursor = getContentResolver().query(CONTENT_PROVIDER_URI, null,
+                                                LOCAL_PAIRS_QUERY, null, null);
+                        displayOnAVD(tv, cursor);
+                    }
+                }
+        );
+
+        gDump.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Cursor cursor = getContentResolver().query(CONTENT_PROVIDER_URI, null,
+                                ALL_PAIRS_QUERY, null, null);
+                        displayOnAVD(tv, cursor);
+                    }
+                }
+        );
     }
 
     @Override
@@ -25,5 +58,27 @@ public class SimpleDhtActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_simple_dht_main, menu);
         return true;
     }
+
+    public void displayOnAVD(TextView tv, Cursor cursor){
+        tv.setText("");
+
+        String cursorToStr = "";
+        final String KEY_COLUMN_NAME = "key";
+        final String VALUE_COLUMN_NAME = "value";
+
+        int keyColumnIndex = cursor.getColumnIndex(KEY_COLUMN_NAME);
+        int valueColumnIndex = cursor.getColumnIndex(VALUE_COLUMN_NAME);
+
+        if(cursor.moveToFirst()) {
+            do {
+                cursorToStr += cursor.getString(keyColumnIndex);
+                cursorToStr += ":";
+                cursorToStr += cursor.getString(valueColumnIndex);
+                cursorToStr += "\n";
+            } while (cursor.moveToNext());
+        }
+
+        tv.setText(cursorToStr);
+    }    //displayOnAVD()
 
 }
